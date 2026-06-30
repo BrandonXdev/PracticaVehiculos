6b56b0f236f87ca0072623c1a492160a5cb289c0
BrandonXdev <brchevezac@est.utn.ac.cr>
2026-06-30 01:45:48 -0600

    Clase motocicleta

---
+++ src/motocicletas/Motocicleta.java
@@ -0,0 +1,18 @@
+
+package motocicletas;
+
+
+public class Motocicleta extends Vehiculo {
+    private int cilindraje;
+
+    public int getCilindraje() {
+        return cilindraje;
+    }
+
+    public Motocicleta(int codigoUnico, String marca, int capacidadPasajeros, double precioPorDiaAlquiler, 
+            boolean estadoDisponibilidad,int cilindraje) {
+    super(codigoUnico, marca, capacidadPasajeros, precioPorDiaAlquiler, estadoDisponibilidad);
+        this.cilindraje = cilindraje;
+    }
+       
+}
