b1e919966aac61fcf88f0ff5d66db4bdb6c8ab15
BrandonXdev <brchevezac@est.utn.ac.cr>
2026-06-30 01:45:25 -0600

    Clase aautomovil

---
+++ src/automoviles/Automovil.java
@@ -0,0 +1,20 @@
+
+package automoviles;
+
+
+
+
+public class Automovil extends Vehiculo {
+    private int numeroPuertas;
+
+    public int getNumeroPuertas() {
+        return numeroPuertas;
+    }
+
+    public Automovil(int codigoUnico, String marca, int capacidadPasajeros, double precioPorDiaAlquiler, 
+            boolean estadoDisponibilidad, int numeroPuertas) {
+        super(codigoUnico, marca, capacidadPasajeros, precioPorDiaAlquiler, estadoDisponibilidad);
+        this.numeroPuertas = numeroPuertas;
+    }
+    
+}
